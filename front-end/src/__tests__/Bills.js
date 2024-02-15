0c23ea21a8b8345677ad982ff0165d7554220de8
Florent <florent.guyard@live.fr>
2024-02-15 22:00:49 +0100

    autre commit avancée dans les tests unitaires et rédaction test E2E

--- front-end/src/__tests__/Bills.js
+++ front-end/src/__tests__/Bills.js
@@ -1,111 +1,60 @@
 /**
  * @jest-environment jsdom
  */
-import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
-import { screen, waitFor, getByTestId } from "@testing-library/dom";
+
+import {screen, waitFor} from "@testing-library/dom"
+import Bills from "../containers/Bills.js";
 import BillsUI from "../views/BillsUI.js"
-import Bills from '../containers/Bills';
-import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
-import { localStorageMock } from "../__mocks__/localStorage.js";
-import { ROUTES } from "../constants/routes.js";
+import { bills } from "../fixtures/bills.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 import router from "../app/Router.js";
-import mockedStore from "../__mocks__/store";
+import userEvent from "@testing-library/user-event";
+import { ROUTES_PATH } from "../constants/routes.js";
+
+// appel du faux storage contenant des factures spécialement conçues pour les tests
+jest.mock("../app/store", () => mockStore)
+$.fn.modal = jest.fn(); 
 
 describe("Given I am connected as an employee", () => {
+
+  function initialisationBills(){
+
+     // Création de la page BillsUI sur le navigateur
+    document.body.innerHTML = BillsUI({ data: bills })
+
+    //Simuler onNavigate
+    const onNavigate = jest.fn(()=>{})
+
+    //Simuler store
+    const store = mockStore
+
+    //Création d'un nouvel utilisateur
+    const userObj = {
+      type:"Employee",
+      email:"employee@test.tld",
+      password:"employee",
+      status:"connected"
+    }
+
+    // création d'un nouveau localstorage dans le navigateur pour les tests
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+
+    // Utilisation du localstorage pour le stockage de l'utilisateur de test
+    window.localStorage.setItem('user', JSON.stringify(userObj))
+
+     // Création d'une nouvelle instance de Bills
+    return new Bills({document, onNavigate, store, localStorage })
+  }
+  
   describe("When I am on Bills Page", () => {
-
-    test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      // création d'un nouveau localstorage dans le navigateur pour les tests
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }));
-
-      // création d'une div et intégration dans la page pour les tests
-      const root = document.createElement("div");
-      root.setAttribute("id", "root");
-      document.body.append(root);
-
-      router();
-
-      window.onNavigate(ROUTES_PATH.Bills);
-      await waitFor(() => screen.getByTestId('icon-window'));
-      const windowIcon = screen.getByTestId('icon-window');
-      
-      // vérification de la classe "active-icon" sur icon-window
-      expect(windowIcon).toHaveClass('active-icon');
-    });
-
-    // test à modifier pour les besoins du projet
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
+    let theBills
+    beforeEach(() =>{
+      //appel de la fonction d'initialisation d'une facture en vue de créer l'environnement de test
+      theBills = initialisationBills()
     })
-
-    test("By clicking on the eye-icon, a modal should open", async () => {
-      
-      // appel du faux storage contenant des factures spécialement conçues pour les tests
-      jest.mock("../app/store", () => mockedStore);
-
-      const onNavigate = pathname => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-       // création d'un nouveau localstorage dans le navigateur pour les tests
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-
-      //Création d'un employee stocké dans le faux storage pour les tests
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
-        // Création d'une nouvelle instance de Bills
-      const billsPage = new Bills({
-        document,
-        onNavigate,
-        store: mockedStore,
-        localStorage: window.localStorage,
-      });
-
-      // Création de la page BillsUI sur le navigateur
-      document.body.innerHTML = BillsUI({ data: bills });
-
-      // Récupération de l'ensemble des icones d'oeil
-      const iconEyes = screen.getAllByTestId("icon-eye");
-
-      // Création d'une version mockée de la fonction gérant le click sur l'icone d'oeil
-      const handleClickIconEye = jest.fn(billsPage.handleClickIconEye);
-
-      // Récupération de la modale dans le DOM
-      const modale = document.getElementById("modaleFile");
-      $.fn.modal = jest.fn(() => modale.classList.add("show")); 
-
-      // Application d'une listener sur chaque icone d'oeil et activation de celui-ci
-      iconEyes.forEach(iconEye => {
-        iconEye.addEventListener("click", () => handleClickIconEye(iconEye));
-        userEvent.click(iconEye);
-
-        expect(handleClickIconEye).toHaveBeenCalled();
-        expect(modale).toHaveClass("show");
-      });
-    
-  });
-
-    test('when clicking on "Nouvelle note de frais" we should navigate to the newBill page ', async () => {
-
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
+  
+    test("Then bill icon in vertical layout should be highlighted", async () => {
 
       // création d'un nouveau localstorage dans le navigateur pour les tests
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -113,74 +62,158 @@ describe("Given I am connected as an employee", () => {
         type: 'Employee'
       }))
 
-      // Création de la page BillsUI sur le navigateur servant de point de départ pour le test
-      document.body.innerHTML = BillsUI({ data : null, error :null, loading : null })
-      
-      // Création d'une nouvelle instance de Bills
-      const billInstance = new Bills({
-        document: document,
-        onNavigate: onNavigate,
-        store: null,
-      });
+      // création d'une div et intégration dans la page pour les tests
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
 
-      // Création d'une version mockée de la fonction gérant le click pour naviguer vers la page newBill
-      const handleClick = jest.fn(billInstance.handleClickNewBill)
+      router()
 
-      // Récupération du bouton permettant de naviguer vers newBill
-      await waitFor(() => screen.getByTestId('btn-new-bill'));
-      const buttonNewBill = screen.getByTestId('btn-new-bill');
+      window.onNavigate(ROUTES_PATH.Bills)
 
-      // Application d'une listener sur le bouton et activation de celui-ci
-      buttonNewBill.addEventListener('click', handleClick())
-      userEvent.click(buttonNewBill)
-      expect(handleClick).toHaveBeenCalled()
-      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
-    
-    });
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
 
-    test("should fetch bills from mock API GET", async () => {
-
-      // appel du faux storage contenant des factures spécialement conçues pour les tests
-      jest.mock("../app/store", () => mockedStore);
-
-      const onNavigate = pathname => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-      // création d'un nouveau localstorage dans le navigateur pour les tests
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-
-      //Création d'un employee stocké dans le faux storage pour les tests
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
-
-      // Création d'une nouvelle instance de Bills
-      const billsInstance = new Bills({
-        document,
-        onNavigate,
-        store: mockedStore,
-        localStorage: window.localStorage,
-      });
-
-      // Création de la page BillsUI sur le navigateur
-      document.body.innerHTML = BillsUI({ data: bills });
-
-      // Surveillance de la fonction mockée getBills
-      const handleGetBills = spyOn(billsInstance, "getBills")
-      await billsInstance.getBills();
-      expect(handleGetBills).toHaveBeenCalled();
-
-      const iconEyes = screen.getAllByTestId("icon-eye");
-      expect(iconEyes).toBeTruthy()
-      const tbody = screen.getByTestId("tbody");
-      expect(tbody).toBeTruthy()
-
+      // vérification de la classe "active-icon" sur icon-window
+      expect(windowIcon.classList.contains("active-icon")).toBe(true)
     })
-  });
-});
+
+    test("Then bills should be ordered from earliest to latest", () => {
+      // test à observer attentivement pour les besoins du projet
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+
+    describe("When click on eye-icon of a bill", ()=>{
+      test("Then render a modal",async()=>{
+        
+          // Récupération de l'ensemble des icones d'oeil
+        const eye_icons = screen.getAllByTestId("icon-eye")
+        
+        // Interaction avec la première des icônes
+        userEvent.click(eye_icons[0])
+        
+        // Verification si le justificatif est bien présent dans le HTML
+        await waitFor(() =>{
+          expect($('#modaleFile').find(".modal-body").innerHTML != '').toBe(true)
+        })
+
+      })
+    })
+        
+    describe('When click on button "Note de frais"',()=>{
+      test('Then handleClickNewBill is called',()=>{
+
+        // Création d'un mock pour la fonction d'appel vers la page pour les nouvelles factures
+        const handleClickNewBill = jest.fn(theBills.handleClickNewBill())
+        
+        // Création et activation d'un listener sur le bouton vers la page newBills
+        const button = screen.getByTestId('btn-new-bill')
+        button.addEventListener('click', handleClickNewBill)
+        userEvent.click(button)
+    
+        expect(handleClickNewBill).toHaveBeenCalled();
+
+        // Annulation du mock précédemment appliqué
+        handleClickNewBill.mockRestore();
+      })
+    })
+
+    // Test d'intégration -> GET
+    describe("When I navigate to Bills Page", () => {
+      test("fetches bills from mock API GET", async () => {
+
+        // Utilisation du faux localstorage afin de créer un utilisateur fictif connecté
+        localStorage.setItem("user", JSON.stringify({ 
+          type:"Employee",
+          email:"a@a",
+          password:"employee",
+          status:"connected"
+        }));
+
+        // création d'une div et intégration dans la page pour les tests
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+
+        router()
+
+        window.onNavigate(ROUTES_PATH.Bills)
+
+        await waitFor(() => {
+          expect(screen.getByText("accepted")).toBeTruthy()
+          expect(screen.getAllByText("pending")).toBeTruthy()
+          expect(screen.getAllByText("refused")).toBeTruthy()
+        })
+      })
+      
+      describe("When an error occurs on API", () => {
+        beforeEach(() => {
+
+          // Création d'un espion sur la fonction bills contenu dans le mockstore
+          jest.spyOn(mockStore, "bills")
+
+          // création d'un nouveau localstorage dans le navigateur pour les tests
+          Object.defineProperty(
+              window,
+              'localStorage',
+              { value: localStorageMock }
+          )
+
+          // Utilisation du faux localstorage afin de créer un utilisateur fictif connecté
+          window.localStorage.setItem('user', JSON.stringify({
+            type:"Employee",
+            email:"a@a",
+            password:"employee",
+            status:"connected"
+          }))
+
+          // création d'une div et intégration dans la page pour les tests
+          const root = document.createElement("div")
+          root.setAttribute("id", "root")
+          document.body.appendChild(root)
+
+          router()
+        })
+        
+        test("fetches bills from an API and fails with 404 message error", async () => {
+    
+          // Création d'une méthode au mockstore provoquant une erreur 404
+          mockStore.bills.mockImplementationOnce(() => {
+            return {
+              list : () =>  {
+                return Promise.reject(new Error("Erreur 404"))
+              }
+            }})
+
+
+          window.onNavigate(ROUTES_PATH.Bills)
+          // utilisation de process pour attendre le prochain roulement de node.js  pour lire la suite du code
+          await new Promise(process.nextTick);
+          const message = screen.getByText(/Erreur 404/)
+          expect(message).toBeTruthy()
+
+        })
+    
+        test("fetches messages from an API and fails with 500 message error", async () => {
+    
+          // Création d'une méthode au mockstore provoquant une erreur 500, donc une erreur qui viendrait du serveur
+          mockStore.bills.mockImplementationOnce(() => {
+            return {
+              list : () =>  {
+                return Promise.reject(new Error("Erreur 500"))
+              }
+            }})
+    
+          window.onNavigate(ROUTES_PATH.Bills)
+          await new Promise(process.nextTick);
+          const message = screen.getByText(/Erreur 500/)
+          expect(message).toBeTruthy()
+        })
+      })
+    })
+  })
+})
---
+++ front-end/src/__tests__/bouine.js
@@ -0,0 +1,188 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { screen, waitFor, getByTestId } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js"
+import Bills from '../containers/Bills';
+import { bills } from "../fixtures/bills.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES } from "../constants/routes.js";
+import router from "../app/Router.js";
+import mockedStore from "../__mocks__/store";
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+
+      // création d'un nouveau localstorage dans le navigateur pour les tests
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+
+      // création d'une div et intégration dans la page pour les tests
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+
+      router();
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId('icon-window'));
+      const windowIcon = screen.getByTestId('icon-window');
+      
+      // vérification de la classe "active-icon" sur icon-window
+      expect(windowIcon).toHaveClass('active-icon');
+    });
+
+    // test à modifier pour les besoins du projet
+    test("Then bills should be ordered from earliest to latest", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+
+    test("By clicking on the eye-icon, a modal should open", async () => {
+      
+      // appel du faux storage contenant des factures spécialement conçues pour les tests
+      jest.mock("../app/store", () => mockedStore);
+
+      const onNavigate = pathname => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+       // création d'un nouveau localstorage dans le navigateur pour les tests
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      //Création d'un employee stocké dans le faux storage pour les tests
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+        // Création d'une nouvelle instance de Bills
+      const billsPage = new Bills({
+        document,
+        onNavigate,
+        store: mockedStore,
+        localStorage: window.localStorage,
+      });
+
+      // Création de la page BillsUI sur le navigateur
+      document.body.innerHTML = BillsUI({ data: bills });
+
+      // Récupération de l'ensemble des icones d'oeil
+      const iconEyes = screen.getAllByTestId("icon-eye");
+
+      // Création d'une version mockée de la fonction gérant le click sur l'icone d'oeil
+      const handleClickIconEye = jest.fn(billsPage.handleClickIconEye);
+
+      // Récupération de la modale dans le DOM
+      const modale = document.getElementById("modaleFile");
+      $.fn.modal = jest.fn(() => modale.classList.add("show")); 
+
+      // Application d'une listener sur chaque icone d'oeil et activation de celui-ci
+      iconEyes.forEach(iconEye => {
+        iconEye.addEventListener("click", () => handleClickIconEye(iconEye));
+        userEvent.click(iconEye);
+
+        expect(handleClickIconEye).toHaveBeenCalled();
+        expect(modale).toHaveClass("show");
+      });
+    
+  });
+
+    test('when clicking on "Nouvelle note de frais" we should navigate to the newBill page ', async () => {
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      // création d'un nouveau localstorage dans le navigateur pour les tests
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      // Création de la page BillsUI sur le navigateur servant de point de départ pour le test
+      document.body.innerHTML = BillsUI({ data : null, error :null, loading : null })
+      
+      // Création d'une nouvelle instance de Bills
+      const billInstance = new Bills({
+        document: document,
+        onNavigate: onNavigate,
+        store: null,
+      });
+
+      // Création d'une version mockée de la fonction gérant le click pour naviguer vers la page newBill
+      const handleClick = jest.fn(billInstance.handleClickNewBill)
+
+      // Récupération du bouton permettant de naviguer vers newBill
+      await waitFor(() => screen.getByTestId('btn-new-bill'));
+      const buttonNewBill = screen.getByTestId('btn-new-bill');
+
+      // Application d'une listener sur le bouton et activation de celui-ci
+      buttonNewBill.addEventListener('click', handleClick())
+      userEvent.click(buttonNewBill)
+      expect(handleClick).toHaveBeenCalled()
+      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
+    
+    });
+
+    test("should fetch bills from mock API GET", async () => {
+
+      // appel du faux storage contenant des factures spécialement conçues pour les tests
+      jest.mock("../app/store", () => mockedStore);
+
+      const onNavigate = pathname => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      // création d'un nouveau localstorage dans le navigateur pour les tests
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+
+      //Création d'un employee stocké dans le faux storage pour les tests
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      // Création d'une nouvelle instance de Bills
+      const billsInstance = new Bills({
+        document,
+        onNavigate,
+        store: mockedStore,
+        localStorage: window.localStorage,
+      });
+
+      // Création de la page BillsUI sur le navigateur
+      document.body.innerHTML = BillsUI({ data: bills });
+
+      // Surveillance de la fonction mockée getBills
+      const handleGetBills = spyOn(billsInstance, "getBills")
+      /*const handleGetBillsBills = jest.spyOn(mockedStore.bills(), "list")*/
+      await billsInstance.getBills();
+      expect(handleGetBills).toHaveBeenCalled();
+
+      const iconEyes = screen.getAllByTestId("icon-eye");
+      expect(iconEyes).toBeTruthy()
+      const tbody = screen.getByTestId("tbody");
+      expect(tbody).toBeTruthy()
+      
+    })
+  });
+});
+
